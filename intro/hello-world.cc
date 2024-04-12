e4e5960d4c8b1db6a0668c889c07bb5b745131d4
Kanwal19 <hkps17@gmail.com>
2024-04-12 09:39:51 +0530

    starting off with c++

---
+++ intro/hello-world.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+/* Multiline comment
+Well its simple printFunc
+Nothing much here
+*/
+void printFunc() { cout << "Hello World!"; }
+
+int main() {
+  printFunc();
+  return 0;
+}
