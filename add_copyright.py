6e2634cbec5f533bb73454304b5b2b8f7c45497f
Kanwal19 <hkps17@gmail.com>
2024-04-13 12:12:11 +0530

    pre-commit hook + practice

---
+++ add_copyright.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+COPYRIGHT_YEAR = "2024"
+AUTHOR = "Harkanwal P Singh"
+COPYRIGHT_TEXT = "// Copyright (c) {} {}. All rights reserved.\n".format(COPYRIGHT_YEAR, AUTHOR)
+
+def add_copyright(file_path):
+    with open(file_path, 'r') as f:
+        content = f.read()
+
+    if COPYRIGHT_TEXT not in content:
+        print("Adding copyright to", file_path)
+        with open(file_path, 'w') as f:
+            f.write(COPYRIGHT_TEXT + content)
+
+if __name__ == "__main__":
+    for file_path in sys.argv[1:]:
+        if file_path.endswith('.cc') or file_path.endswith('.cpp'):
+            add_copyright(file_path)
